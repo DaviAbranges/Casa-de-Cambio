52692ed1aebb0ef526e25df415da4d43fb34482b
Davi <daviabrabranges123@gmail.com>
2024-05-28 02:09:53 -0300

    Update Project

--- src/main.js
+++ src/main.js
@@ -1,21 +1,29 @@
-import './style.css';
-import Swal from 'sweetalert2';
+import "./style.css";
+import Swal from "sweetalert2";
 
 // Capturar elementos
-const btn = document.getElementById('btnPesquisar');
-const container = document.getElementById('container'); // elemento pai
-let emoji = '\u{1F4B0}';
-
+const btn = document.getElementById("btnPesquisar");
+const container = document.getElementById("container"); // elemento pai
+let emoji = "\u{1F4B0}";
 
 // Event Listenner
-btn.addEventListener('click', (event) => {
+btn.addEventListener("click", (event) => {
   event.preventDefault();
-  container.innerText = '';
-  const moeda = document.getElementById('inputMoeda').value.toUpperCase();
+  container.innerText = "";
+  const moeda = document.getElementById("inputMoeda").value.toUpperCase();
 
-  const url = `https://api.exchangerate.host/latest?base=${moeda}`;
+  const myHeaders = new Headers();
+  myHeaders.append("apikey", "bxSXTqAiS0znXqA5KSCAg6bmUIpEKZhz");
 
-  fetch(url)
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+    headers: myHeaders,
+  };
+
+  const url = `https://api.apilayer.com/exchangerates_data/latest?symbols=&base=${moeda}`;
+
+  fetch(url, requestOptions)
     .then((promise) => promise.json())
     .then((data) => {
       validarMoeda(moeda, data.rates);
@@ -23,29 +31,29 @@ btn.addEventListener('click', (event) => {
     })
     .catch((error) => {
       Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
+        icon: "error",
+        title: "Oops...",
         text: error.message,
       });
     });
 });
 
 function validarMoeda(moeda, obj) {
-  const array = [];
-  for (const chave in obj) { array.push(chave); }
-  if (moeda === '') throw new Error('Você precisa passar uma moeda!');
-  if (array.includes(moeda) === false) throw new Error('Moeda invávida');
+  if (!moeda) throw new Error("Você precisa passar uma moeda!");
+  if (!obj) throw new Error("Moeda inválida");
 }
 
 function listarMoedas(moeda, obj) {
-  const titulo = document.createElement('h2');
+  const titulo = document.createElement("h2");
   titulo.innerText = `Valores referentes a 1 ${moeda}`;
   container.appendChild(titulo);
 
   for (const chave in obj) {
-    const caixa = document.createElement('div');
-    caixa.className = 'moedas';
-    caixa.innerHTML = `${emoji} ${chave}  - <span>${obj[chave].toFixed(2)}</span>`;
+    const caixa = document.createElement("div");
+    caixa.className = "moedas";
+    caixa.innerHTML = `${emoji} ${chave}  - <span>${obj[chave].toFixed(
+      2
+    )}</span>`;
     container.appendChild(caixa);
   }
 }
